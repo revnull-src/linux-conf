9357773ea4f56fd042b451cdd43b05909998388c
Ray Ramirez <rramirez@revnull.com>
2015-03-29 09:26:19 -0700

    Switch to curl.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ linux-conf
 ==========
 
 My Linux dot files. Nothing to see here.
-1
+
--- update_files.sh
+++ update_files.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-chmod 755 git-prompt.sh
-mv git-prompt.sh .git-prompt.sh
+# URL,file_name,make_exe
+CurlThese="
+https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh|.git-prompt.sh|1
+https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash|.git-completion.bash|1
+https://raw.githubusercontent.com/vim-scripts/twilight256.vim/master/colors/twilight256.vim|twilight256.vim|0
+"
 
-wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-chmod 755 git-completion.bash
-mv git-completion.bash .git-completion.bash
+for List in $CurlThese
+do
+    Url=`echo $List | cut -d'|' -f1`
+    File=`echo $List | cut -d'|' -f2`
+    Exe=`echo $List | cut -d'|' -f3`
+
+    echo Downloading $File
+    curl -# -o $File $Url
+    if [ $Exe -eq 1 ]; then
+        chmod 755 $File
+    fi 
+    echo
+done
 
